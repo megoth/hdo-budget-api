848f702beed86d4bc30e2baf42ca6c65ec695336
Arne <arne.hassel@rr-research.no>
2014-03-14 19:14:59 +0100

    A bit more work on models

--- config/models.js
+++ config/models.js
@@ -1,17 +1,17 @@
 var Schema = require('jugglingdb').Schema;
 
 module.exports = function () {
-    var schema = new Schema('postgres', {
-        database: 'postgres',
-        username: 'postgres',
-        password: 'test'
-         // host: 'localhost',
-         // port: 5432,
-         // password: s.password,
-         // database: s.database,
-         // ssl: true,
-         // debug: false
-    });
-    
-    require('../models/User')(schema);
+  var schema = new Schema('postgres', {
+    username: 'postgres',
+    password: 'test',
+    database: 'postgres'
+    // host: 'localhost',
+    // port: 5432,
+    // password: s.password,
+    // database: s.database,
+    // ssl: true,
+    // debug: false
+  });
+  
+  require('../models/User')(schema);
 };
--- models/User.js
+++ models/User.js
@@ -1,18 +1,27 @@
 var User = null;
 
 module.exports = function (schema) {
-    if (User) {
-        return User;
-    }
-    
-    if (!schema) {
-        throw new Error('No schema given!');
-    }
-    
-    User = schema.define('User', {
-        email: String
-    });
-    console.log('Defined user schema');
-    
+  if (User) {
     return User;
+  }
+  
+  if (!schema) {
+    throw new Error('No schema given!');
+  }
+  
+  User = schema.define('User', {
+    email: String
+  });
+  console.log('Defined user schema');
+  
+  User.create({
+    email: 'arne.hassel@gmail.com'
+  }, function () {
+    console.log("CREATED USER", arguments);
+    User.count(function () {
+      console.log("USERS", arguments);
+    });
+  });
+  
+  return User;
 };

96debc8c2c303a332ff0f0320bcba895bace10e6
Arne <arne.hassel@rr-research.no>
2014-03-15 13:55:19 +0100

    Partial work on budgets

--- config/routes.js
+++ config/routes.js
@@ -1,10 +1,12 @@
 var routes = require('../routes');
 var user = require('../routes/user');
 var admin = require('../routes/admin');
+var budget = require('../routes/budget');
 
-module.exports = function (app, passport) {
+module.exports = function (app, passport, auth) {
   app.get('/', routes.index);
   app.get('/admin', admin.list);
+  app.get('/budgets/create', budgets.create);
   app.get('/users', user.list);
   app.get('/logout', user.logout);
   app.get('/auth/google', passport.authenticate('google'));
